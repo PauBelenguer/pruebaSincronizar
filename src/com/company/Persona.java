1139443aa94e239fa4a653e2f8aaf286c6879182
pau belenguer <pau.belenguer.doria@cieep.com>
2020-04-30 11:04:01 +0200

    prueba2

--- src/com/company/Persona.java
+++ src/com/company/Persona.java
@@ -1,4 +1,24 @@
 package com.company;
 
 public class Persona {
+    private int id;
+
+    public Persona(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                '}';
+    }
 }
